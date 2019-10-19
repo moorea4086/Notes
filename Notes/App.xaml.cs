3484c7e8728beb19a083f443b1c9be77c5c1b072
Alexander Moore <mooreac@susqu.edu>
2019-10-19 13:58:25 -0400

    Changed App.xaml.cs, NoteEntryPage.xaml.cs, NotesPage.xaml.cs

--- Notes/App.xaml.cs
+++ Notes/App.xaml.cs
@@ -1,19 +1,45 @@
 ﻿using System;
 using System.IO;
 using Xamarin.Forms;
+using Notes.Data;
 
 namespace Notes
 {
     public partial class App : Application
     {
-        public static string FolderPath { get; private set; }
+        static NoteDatabase database;
+
+        public static NoteDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new NoteDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Notes.db3"));
+                }
+                return database;
+            }
+        }
 
         public App()
         {
             InitializeComponent();
-            FolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
             MainPage = new NavigationPage(new NotesPage());
         }
-        // ...
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
     }
 }
--- Notes/NoteEntryPage.xaml.cs
+++ Notes/NoteEntryPage.xaml.cs
@@ -15,31 +15,15 @@ namespace Notes
         async void OnSaveButtonClicked(object sender, EventArgs e)
         {
             var note = (Note)BindingContext;
-
-            if (string.IsNullOrWhiteSpace(note.Filename))
-            {
-                // Save
-                var filename = Path.Combine(App.FolderPath, $"{Path.GetRandomFileName()}.notes.txt");
-                File.WriteAllText(filename, note.Text);
-            }
-            else
-            {
-                // Update
-                File.WriteAllText(note.Filename, note.Text);
-            }
-
+            note.Date = DateTime.UtcNow;
+            await App.Database.SaveNoteAsync(note);
             await Navigation.PopAsync();
         }
 
         async void OnDeleteButtonClicked(object sender, EventArgs e)
         {
             var note = (Note)BindingContext;
-
-            if (File.Exists(note.Filename))
-            {
-                File.Delete(note.Filename);
-            }
-
+            await App.Database.DeleteNoteAsync(note);
             await Navigation.PopAsync();
         }
     }
